a42210c1f52b8a94c129b4faa870e140d78ecc2f
Boris <boris.lukanovic17@gmail.com>
2020-04-21 10:07:24 +0200

    refactor and make appBar sticky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,15 @@
 import React from "react"
-import PropTypes from "prop-types"
 import SwipeableViews from "react-swipeable-views"
+
+import { TabPanel, a11yProps } from "./TabPanel"
 //material-ui stuff
 import { makeStyles, useTheme } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
-import Typography from "@material-ui/core/Typography"
-import Box from "@material-ui/core/Box"
 
-import Emojis from "../components/Emoji/EmojiApp"
-import Recipes from "../components/Recipe/RecipeMain"
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <Typography
-      component='div'
-      role='tabpanel'
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  )
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-}
-
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  }
-}
+import Emojis from "../../components/Emoji/EmojiApp"
+import Recipes from "../../components/Recipe/RecipeMain"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -64,7 +33,7 @@ export default function FullWidthTabs() {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static' color='default'>
+      <AppBar position='sticky' color='default' top='0'>
         <Tabs
           value={value}
           onChange={handleChange}
